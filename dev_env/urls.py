0aa0f6b5f2bd5d90efbb12b96d0ee052d0d8a0b6
Deekshithmmm <deekshithmdeekshi558@gmail.com>
2026-03-08 15:14:55 +0530

    Feedback form integrated with GitHub issues

--- dev_env/urls.py
+++ dev_env/urls.py
@@ -2,7 +2,28 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
+from django_ledger.views.feedback import BugReportView, RequestNewFeatureView
+
+
 urlpatterns = [
+
+    # Django admin panel
     path('admin/', admin.site.urls),
-    path('', include('django_ledger.urls', namespace='django_ledger')),
+
+    # Django Ledger main URLs
+    path('', include(('django_ledger.urls', 'django_ledger'), namespace='django_ledger')),
+
+    # Feedback endpoints (send issues to GitHub)
+    path(
+        'feedback/bug/',
+        BugReportView.as_view(),
+        name='bug-report'
+    ),
+
+    path(
+        'feedback/feature/',
+        RequestNewFeatureView.as_view(),
+        name='feature-request'
+    ),
+
 ]
